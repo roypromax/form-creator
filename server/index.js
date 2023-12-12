9ac0dcd42d7b64659993b851933b2ebb10ba7147
roypromax <roy19rishabh@gmail.com>
2023-12-13 00:40:37 +0530

    fix CORS error

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const connection = require("./configs/db");
 const formRouter = require("./routes/form.routes");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
